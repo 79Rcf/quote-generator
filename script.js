ef74ce2b171d4ea5b164cc6614bfdeb9ad1cd3a3
Ulrich Killian <fastfrightshipping@gmail.com>
2025-11-18 12:05:32 +0100

    setting up my repo

---
+++ script.js
@@ -0,0 +1,7 @@
+async function getJokes() {
+    let response = await fetch ('https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark,Pun,Spooky,Christmas?amount=10')
+    let data = await response.json();
+    console.log(data);
+}
+getJokes();
+
